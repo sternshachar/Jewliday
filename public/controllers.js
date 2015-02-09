d1a409b1f6dd8c24abf0d750629b8700ca53fc11
Stern <stern@Sterns-MBP.Home>
2015-02-09 11:54:57 +0200

    setting up angular, bootstrap & express server

---
+++ public/controllers.js
@@ -0,0 +1,5 @@
+angular.module("jewApp")
+
+.controller("mainCtrl",function(){
+	$scope.proof = "stuff";
+})
---
+++ server.js
@@ -0,0 +1,6 @@
+/* --- setting up an express server --- */
+var express = require('express');
+var express = express();
+app.use(express.static('public'));
+app.listen(8080);
+/* --- express server setup --- */
