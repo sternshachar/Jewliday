9251a499f427728b01eebafe5b4519dfe525a1e3
Stern <stern@Sterns-MBP.Home>
2015-02-10 20:54:32 +0200

    authent return name

--- public/authenticate.js
+++ public/authenticate.js
@@ -26,7 +26,7 @@ passport.deserializeUser(function(id,done){
 	console.log('deserial works');
 	User.findOne({"_id": id},function(err,user){
 		if(err) return console.error(err);
-		done(null, {id: id, name: user.name});
+		done(null, {id: id, name: user.first_name});
 	});
 });
 
