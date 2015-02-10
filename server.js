4bb6da1bbf873698048a908e7ad9c3546733880e
Stern <stern@Sterns-MBP.Home>
2015-02-10 17:26:54 +0200

    new user adding to DB

--- server.js
+++ server.js
@@ -6,8 +6,27 @@ app.use(express.static('public'));
 app.listen(8080);
 /* --- express server setup --- */
 app.use(bodyParser());
+
+/* --- connect to mongoDB --- */
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/jewliday');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console,'connection error: '));
+/* --- end of connect to mongoDB --- */
+
+var usersSchema = [];
+db.once('open',function(callback){
+	usersSchema = mongoose.Schema({first_name: String, last_name: String, email: String, password: String, agree: Boolean},{collection:'users'});
+	User = mongoose.model('users',usersSchema);
+});
+
+
 app.post('/signup',function(req,res){
 	var user = req.body;
-	console.log(user)
-	res.json(user);
+	var newUser = new User(user);
+	newUser.save(function(err,newUser){
+		if(err) return console.error(err);
+		console.log(newUser);
+	})
+	res.status(201).json(newUser);
 });
