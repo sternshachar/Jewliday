6dacc2af9f1cebfa705f320967bf12c4db26269f
Stern <stern@Sterns-MBP.Home>
2015-02-11 23:00:39 +0200

    condition if email already exists

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.post('/signup',function(req,res){
 
 	User.findOne({"email": req.body.email}, function(err,oldUser){
 		if(user.email == oldUser.email){
-			res.json(
+			return res.json(
 				{
 					isAuthenticated: req.isAuthenticated(),
 					message: 'User with this email already signed up'
