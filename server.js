a9dca9d234829feb18e2f043a30c33cfd58db6a4
Stern <stern@Sterns-MBP.Home>
2015-02-11 11:45:37 +0200

    changed deser.. log

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(cookieParser());
 app.use(expressSession({
  	secret: process.env.SESSION_SECRET || 'secret',
  	resave: false,
+ 	coockie:{secure: false},
  	saveUninitialized: false
 }));
 
