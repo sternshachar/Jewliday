80811121dd5150a013603f57ea2f96959eeb9146
Stern <stern@Sterns-MBP.Home>
2015-02-11 11:37:08 +0200

    changed deser.. log

--- public/authenticate.js
+++ public/authenticate.js
@@ -23,7 +23,7 @@ passport.serializeUser(function(user,done){
 });
 
 passport.deserializeUser(function(id,done){
-	console.log(id);
+	console.log('deserialize works');
 	User.findOne({"_id": id},function(err,user){
 		if(err) return console.error(err);
 		done(null, {id: id, name: user.email});
--- server.js
+++ server.js
@@ -21,13 +21,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(function(req, res, next) {
-  res.header['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, X-AUTHENTICATION, X-IP, Content-Type, Accept';
-  res.header ['Access-Control-Allow-Credentials'] = true;
-  res.header ['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS';
-  next()
-});
-
-app.use(function(req, res, next) {
 
     console.log('-- session --');
     console.dir(req.session);
