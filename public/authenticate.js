eb8d51a60cd9136b1ce133b9fd29de706f9c6989
Stern <stern@Sterns-MBP.Home>
2015-02-11 17:33:41 +0200

    checking if user exists

--- public/authenticate.js
+++ public/authenticate.js
@@ -6,12 +6,13 @@ passport.use(new passportLocal(function(username,password,done){
 	User.findOne({"email": username },function(err,user){
 		console.log(user);
 		if(err) return console.error(err);
+		if(!user) return done(null, false, { message: 'Wrong email.' });
 		if(!(user == undefined) && user.password == password){
 			console.log('found');
 			done(null,{id: user._id ,name: username});
 		} else{
 			console.log('not found');
-			done(null,null);
+			return done(null, false, { message: 'Incorrect password.' });
 		}
 	});
 
