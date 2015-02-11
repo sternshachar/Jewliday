8f0fd3866a72c8f17237b5f190adca5d28714e44
Stern <stern@Sterns-MBP.Home>
2015-02-11 18:00:34 +0200

    checking if user exists

--- server.js
+++ server.js
@@ -58,16 +58,14 @@ app.get('/login', function(request,response){
 	});
 
 
-app.post('/login', function(request,response){
-		passport.authenticate('local', function(err, user, info) {
-			console.log(info);
-			response.json(
-				{
-					isAuthenticated: request.isAuthenticated(),
-					user: request.user
-				}
-			);
-		});
+app.post('/login',passport.authenticate('local'), function(request,response){
+		console.log(request.user);
+		response.json(
+			{
+				isAuthenticated: request.isAuthenticated(),
+				user: request.user
+			}
+		);
 	})
 	.get(function(request,response){
 		response.json(
