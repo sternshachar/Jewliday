d8b939c131d01b39bd3fc838e672baafb625ced2
Stern <stern@Sterns-MBP.Home>
2015-02-12 11:28:40 +0200

    added require to mongo.js

--- auth.js
+++ auth.js
@@ -1,10 +1,9 @@
 var passport = require('passport');
 var passportLocal = require('passport-local').Strategy;
-var User = require('./mongo').User;
+var mongoose = require('mongoose');
 
-console.log(User);
 passport.use(new passportLocal(function(username,password,done){
-	User.findOne({"email": username },function(err,user){
+	mongoose.model('users').findOne({"email": username },function(err,user){
 		// console.log(user);
 		if(err) return console.error(err);
 		if(!user) return done(null, false, { message: 'Wrong E-mail' });
@@ -26,7 +25,7 @@ passport.serializeUser(function(user,done){
 
 passport.deserializeUser(function(id,done){
 	console.log('deserialize works');
-	User.findOne({"_id": id},function(err,user){
+	mongoose.model('users').findOne({"_id": id},function(err,user){
 		if(err) return console.error(err);
 		done(err, user);
 	});
