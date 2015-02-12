d425dde43419b91532ba72cae03b2568acc055e2
Stern <stern@Sterns-MBP.Home>
2015-02-12 10:35:16 +0200

    Added automatic timestamps to users

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.use(flash());
 
 /* --- connect to mongoDB --- */
 var mongoose = require('mongoose');
+var timestamps = require('mongoose-timestamp');
 mongoose.connect('mongodb://localhost/jewliday');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console,'connection error: '));
@@ -35,7 +36,8 @@ db.on('error', console.error.bind(console,'connection error: '));
 console.log('Connected to mongoDB!')
 var usersSchema = [];
 db.once('open',function(callback){
-	usersSchema = mongoose.Schema({firstName: String, lastName: String, email: String, password: String, agree: Boolean},{collection:'users'});
+	usersSchema = mongoose.Schema({firstName: String, lastName: String, email: String, password: String, agree: Boolean, signupData: Timestamp },{collection:'users'});
+	usersSchema.plugin(timestamps);
 	User = mongoose.model('users',usersSchema);
 });
 
