19b21fec93528b3cf57f46eb8541b223e2d96768
Stern <stern@Sterns-MBP.Home>
2015-02-13 13:00:37 +0200

    user directive

--- public/directives.js
+++ public/directives.js
@@ -101,7 +101,7 @@ angular.module('jewApp')
 	return {
 		restrict: 'E',
 		templateUrl: function(elem,attrs){
-			console.log($eval(attrs['content']);
+			console.log($eval(attrs['content']));
 			switch(attrs['content']){
 				case 'profile':
 					return 'views/profile.html';
