0412be86b0946ef49b24f19ce53209b2979ebc22
Stern <stern@Sterns-MBP.Home>
2015-02-15 14:05:48 +0200

    inbox fetch

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.get('/inbox/:id', function(req,res){
 	var Inbox = mongoose.model('inboxes');
 	var id = req.params.id;
 	console.log('ownerId ' + id);
-	Inbox.find({},function(err,messages){
+	Inbox.find({"data":"1/2/3"},function(err,messages){
 		console.log(messages);
 		res.json(messages);
 	});
