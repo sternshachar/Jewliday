4ef24cbd3c5c227afcb4ec72ff0dc62df0b1da2a
Stern <stern@Sterns-MBP.Home>
2015-02-15 12:28:01 +0200

    inbox fetch

--- mongo.js
+++ mongo.js
@@ -16,11 +16,28 @@ var Inbox = {};
 var ObjectId = mongoose.Types.ObjectId();
 
 db.once('open',function(callback){
-	usersSchema = mongoose.Schema({firstName: String, lastName: String, email: String, password: String, agree: Boolean },{collection:'users'});
+	usersSchema = mongoose.Schema({
+		firstName: String,
+		lastName: String,
+		email: String,
+		password: String,
+		agree: Boolean },
+		{collection:'users'}
+	);
 	usersSchema.plugin(timestamps);
+	inboxSchema = mongoose.Schema({
+		ownerId: ObjectId,
+		messages: [
+			sender: {type: ObjectId, ref: User},
+			subject: String,
+			content: String ,
+			sent: {type: Date, default: Date.now}
+		]},
+		{collection:'inboxes'}
+	);
+
 	User = mongoose.model('users',usersSchema);
 
-	inboxSchema = mongoose.Schema({ownerId: ObjectId, messages: {sender: ObjectId, subject: String, content: String , sent: {type: Date, default: Date.now} }},{collection:'inboxes'});
 	Inbox = mongoose.model('inboxes',usersSchema);
 });
 
