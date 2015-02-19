a1d694e02db37610bfc6030321ce7495fa84ac16
Stern <stern@Sterns-MacBook-Pro.local>
2015-02-19 23:02:07 +0200

    adding google-map-angular

--- public/controllers.js
+++ public/controllers.js
@@ -8,6 +8,7 @@ angular.module("jewApp")
 })
 .controller("mainCtrl",function($scope,$interval,$http,$location,$state,userData,uiGmapGoogleMapApi){
 	$scope.username = $http.get(userData.url + '/login').then(function(data){
+		console.log(data);
 		return data.user.firstName;
 	});
 	$scope.isAuth = $http.get(userData.url + '/login').then(function(data){
