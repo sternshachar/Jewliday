a2fae9e40471e2d4b43693b13afd1f6ded1a98cd
Stern <stern@Sterns-MacBook-Pro.local>
2015-02-19 20:27:07 +0200

    adding google-map-angular

--- public/controllers.js
+++ public/controllers.js
@@ -4,13 +4,13 @@ angular.module("jewApp")
 	this.getAdderss = function(){
 		$http.get(this.url + '/login').
 		success(function(data){
-			console.log(data);
 			var city = data.user.house.city.split(", ").join("+");
 			this.address =   data.user.house.homeNumber + '+' + data.user.house.street +',' + '+' + city;
 			$http.get('http://maps.google.com/maps/api/geocode/json?address='+ this.address +'&sensor=false')
 				    .success(function(mapData) {
+				    	console.log(this.mapData.results[0]);
 						   return mapData;
-					       console.log(this.mapData.results[0]);
+					       
 					})
 			})
 	}
