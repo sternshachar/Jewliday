4d8f2f060bcf80fc8be6cdc2892321de03e3c07f
Stern <stern@Sterns-MacBook-Pro.local>
2015-02-19 17:00:20 +0200

    adding google-map-angular

--- public/controllers.js
+++ public/controllers.js
@@ -30,7 +30,7 @@ angular.module("jewApp")
 		$http.get('http://maps.google.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&sensor=false')
 		    .success(function(mapData) {
 				   $scope.mapData = mapData;
-			       console.log($scope.mapData);
+			       console.log($scope.mapData.results[0]);
 			})
 			.error(function(err){
 				console.error(err);
