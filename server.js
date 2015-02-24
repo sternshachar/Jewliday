ad8bbc465d89150822c739ed4f48826989a5e135
Stern <stern@Sterns-MBP.Home>
2015-02-24 09:51:26 +0200

    SAVED TO EC2 and then UPLOADED TO S3

--- server.js
+++ server.js
@@ -169,13 +169,14 @@ app.post('/upload', function(req, res) {
     var form = new formidable.IncomingForm();
     form.parse(req, function(err, fields, files) {
         // `file` is the name of the <input> field of type `file`
+        var id = files.id;
         var old_path = files.file.path,
             file_size = files.file.size,
             file_ext = files.file.name.split('.').pop(),
             index = old_path.lastIndexOf('/') + 1,
             file_name = old_path.substr(index),
             new_path = path.join(process.env.PWD, '/uploads/', file_name + '.' + file_ext);
- 			console.log(files.file,path);
+ 			console.log(id);
         fs.readFile(old_path, function(err, data) {
             fs.writeFile(new_path, data, function(err) {
                 fs.unlink(old_path, function(err) {
