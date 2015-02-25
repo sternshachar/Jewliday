d1ab54c056098fc8a8c0b2ee74e85c8f90be4933
Stern <stern@Sterns-MBP.Home>
2015-02-25 18:48:07 +0200

    upload success feedback

--- public/controllers.js
+++ public/controllers.js
@@ -156,7 +156,7 @@ angular.module("jewApp")
 			{ type: 'pic6'    , name: 'Another Photo',done: false, show: false}
 	];
 	$scope.anotherPic = function(){
-		if($scope.limit > 6)
+		if($scope.limit < 6)
 			$scope.limit = $scope.limit + 1;
 	}
 
