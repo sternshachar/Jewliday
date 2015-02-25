e9b1390653da73912b1824d0628bab6bd703a5df
Stern <stern@Sterns-MBP.Home>
2015-02-25 12:37:12 +0200

    upload success feedback

--- public/controllers.js
+++ public/controllers.js
@@ -152,6 +152,7 @@ angular.module("jewApp")
 		var result = document.getElementById("profileFrame");
 		var wrappedResult = angular.element(result);
 		var content = wrappedResult.text();
+		console.log(content);
 	}
 
 	$scope.chooseFile = function(picType) {
