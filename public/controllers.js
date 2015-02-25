90a4f713319848a41b0fdedc2c602687bad36610
Stern <stern@Sterns-MBP.Home>
2015-02-25 15:54:54 +0200

    upload success feedback

--- public/controllers.js
+++ public/controllers.js
@@ -160,7 +160,9 @@ angular.module("jewApp")
                     file: file
                 }).progress(function (evt) {
                     var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-                    $scope.progress = progressPercentage;
+                    $scope.getPercentage = function () {
+					    return progressPercentage;
+					}
                     console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
                 }).success(function (data, status, headers, config) {
                     console.log('file ' + config.file.name + 'uploaded. Response: ' + data);
