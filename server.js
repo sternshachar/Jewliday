4a9d39bdfcc8cfa826c4c6ca071e420eee412fbf
Stern <stern@Sterns-MBP.Home>
2015-02-25 14:13:10 +0200

    upload success feedback

--- server.js
+++ server.js
@@ -173,6 +173,7 @@ app.post('/upload/:type', function(req, res) {
     var form = new formidable.IncomingForm();
     form.parse(req, function(err, fields, files) {
         // `file` is the name of the <input> field of type `file`
+        console.log(files);
         var id = fields.id;
         var old_path = files.file.path,
             file_size = files.file.size,
