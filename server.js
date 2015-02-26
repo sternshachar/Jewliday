399e9787bcfb28929316efa0df7350a5afad6d2f
Stern <stern@Sterns-MBP.Home>
2015-02-26 10:30:10 +0200

    adding date to pic name on s3

--- server.js
+++ server.js
@@ -157,7 +157,7 @@ function uploadFile(remoteFilename, file, id) {
 				if(err) return console.error(err);
 				user.set(
 					'photos.' + type,
-					'https://s3-us-west-2.amazonaws.com/jewliday/' + id + '/' + type + Date.parse(date) + file_ext
+					'https://s3-us-west-2.amazonaws.com/jewliday/' + id + '/' + type + Date.parse(date) + '.' + file_ext
 					);
 				user.save(function(err,user){
 					if(err) return console.error(err);
