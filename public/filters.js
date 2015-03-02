f026a74458599e566c3cbb627aba39c262479ae5
Stern <stern@Sterns-MBP.Home>
2015-03-02 15:40:29 +0200

    filter well

--- public/filters.js
+++ public/filters.js
@@ -1,12 +1,40 @@
 angular.module("jewApp")
-.filter("fire", function () {
-	return function (items) {
+.filter("amenFilter", function () {
+	return function (items, filterObject) {
 		var resultArr = [];
+		var noFilterNeeded = true;
+
+		for(var propertyName in filterObject) { //if all filter is false all items go back
+			if(filterObject[propertyName] == true)
+				noFilterNeeded = false;
+				break;
+		}
+
+		if(noFilterNeeded){
+			return items;
+		}
+
 		angular.forEach(items, function (item) {
-			if (item.done == false || showComplete == true) {
-			resultArr.push(item);
+			var insert = false;
+			for(var propertyName in filterObject) {
+				if(filterObject[propertyName] == true ){
+					if(item.house.amenities[propertyName] == true){
+						insert = true
+					} else {
+						insert = false;
+						break;
+					}
+				}
+
+			   // propertyName is what you want
+			   // you can get the value like this: myObject[propertyName]
 			}
-		});
+		if(insert) resultArr.push(item);
+		}
 		return resultArr;
-	}
+	});
 });
+
+			// if (item.done == false || showComplete == true) {
+			// resultArr.push(item);
+			// }
