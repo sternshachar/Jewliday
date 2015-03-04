9bd24bd1b6863ae5ecd665d12917c8d9a5904a34
Stern <stern@Sterns-MBP.Home>
2015-03-04 13:07:01 +0200

    try open new tab

--- public/jewApp.js
+++ public/jewApp.js
@@ -138,6 +138,6 @@ angular.module("jewApp",["ngAnimate","ui.bootstrap","ngRoute","ngAutocomplete","
 						templateUrl: 'views/listHome.photos.html'
 					})
 					.state('browse',{
-						url:'/'
+						url:'views/users.home.html'
 					})
 			})
