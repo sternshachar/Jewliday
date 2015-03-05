b634daa6875c1adc86244a4bea4e269b749a0f6b
Stern <stern@Sterns-MacBook-Pro.local>
2015-03-05 10:38:47 +0200

    seperate factory for home search

--- public/services.js
+++ public/services.js
@@ -101,6 +101,7 @@ angular.module("jewApp")
 			},
 
 			homeSelect: function(home){
+				console.log(homeSelected);
 				homeSelected = home;
 				console.log(homeSelected);
 				return homeSelected;
