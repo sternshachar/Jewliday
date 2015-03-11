880e5c7ff7a4ddf6803571bba1de971d91813303
Stern <stern@Sterns-MBP.Home>
2015-03-11 12:12:45 +0200

    photos loaded in homeCtrl from userservice

--- public/controllers.js
+++ public/controllers.js
@@ -207,7 +207,7 @@ angular.module("jewApp")
         }
     };
 })
-.controller('homeCtrl', function($scope,$http,$state,uiGmapGoogleMapApi,appData,addressData,photos){
+.controller('homeCtrl', function($scope,$http,$state,uiGmapGoogleMapApi,appData,userService,addressData,photos){
 	$scope.browseMode = false;
 
 	var amenities = {};
