5ca01dc874db8adace91eaa0aba43c435478aa91
Stern <stern@Sterns-MBP.Home>
2015-03-15 12:21:18 +0200

    no resolve for home

--- public/controllers.js
+++ public/controllers.js
@@ -258,33 +258,34 @@ angular.module("jewApp")
 			}
 		}
 	}
-	userService.getData()
-		.then(function(data){
-			console.log(data);
-			$scope.mapData = data.homeData.location;
-			$scope.map = {center: {latitude: $scope.mapData.lat,
-     					   longitude: $scope.mapData.lng }, zoom: 14 };
-     			$scope.marker = {
-					id: 0,
-					coords: {
-					  latitude: $scope.mapData.lat,
-					  longitude: $scope.mapData.lng
-					},
-					options: { draggable: false },
-					events: {
-					  click: function (marker, eventName, args) {
-					    var lat = marker.getPosition().lat();
-					    var lon = marker.getPosition().lng();
+	if(!$scope.userData.isListed)
+		userService.getData()
+			.then(function(data){
+				console.log(data);
+				$scope.mapData = data.homeData.location;
+				$scope.map = {center: {latitude: $scope.mapData.lat,
+	     					   longitude: $scope.mapData.lng }, zoom: 14 };
+	     			$scope.marker = {
+						id: 0,
+						coords: {
+						  latitude: $scope.mapData.lat,
+						  longitude: $scope.mapData.lng
+						},
+						options: { draggable: false },
+						events: {
+						  click: function (marker, eventName, args) {
+						    var lat = marker.getPosition().lat();
+						    var lon = marker.getPosition().lng();
 
-					    $scope.marker.options = {
-					    	labelClass: "marker-labels",
-					      content: "<img class='map-info-pic' ng-src='" + $scope.photosUrl.profile + "'>",
-					      zIndex: 99999
-					    };
-					  }
+						    $scope.marker.options = {
+						    	labelClass: "marker-labels",
+						      content: "<img class='map-info-pic' ng-src='" + $scope.photosUrl.profile + "'>",
+						      zIndex: 99999
+						    };
+						  }
+						}
 					}
-				}
-		})
+			})
 	
 	$scope.options = {scrollwheel: false};
 
