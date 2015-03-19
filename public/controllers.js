597fa53847b3d626b22d77d4268f9948d383fdbb
Stern <stern@Sterns-MBP.Home>
2015-03-19 15:38:17 +0200

    general loading bar removed

--- public/controllers.js
+++ public/controllers.js
@@ -155,7 +155,8 @@ angular.module("jewApp")
 	$scope.files = {};
 	$scope.progress = [0,0,0,0,0,0,0,0];
 	$scope.bar = [{width: 0 + '%'},{width: 0 + '%'},{width: 0 + '%'},{width: 0 + '%'},
-				  {width: 0 + '%'},{width: 0 + '%'},{width: 0 + '%'},{width: 0 + '%'}]
+				  {width: 0 + '%'},{width: 0 + '%'},{width: 0 + '%'},{width: 0 + '%'}];
+				  console.log($scope.bar)
 	$scope.details = "";
 
     $scope.saveHome = function(){
@@ -220,7 +221,7 @@ angular.module("jewApp")
                 }).progress(function (evt) {
                     var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
                     $scope.progress[index] = progressPercentage;
-                    $scope.bar[index] = {width: progressPercentage + '%'};
+                    // $scope.bar[index] = {width: progressPercentage + '%'};
                     console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
                 }).success(function (data, status, headers, config) {
                     console.log('file ' + config.file.name + 'uploaded. Response: uploaded');
