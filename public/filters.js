fabc840c66790e72f0f8abd2279f757711e53f78
Stern <stern@Sterns-MBP.Home>
2015-04-15 16:56:23 +0300

    adding favorite destinations

--- public/filters.js
+++ public/filters.js
@@ -137,7 +137,7 @@ angular.module("jewApp")
 			var randNum = [-1, -1 ,-1];
 			for (var i = 0; i < randNum.length; i++) {
 				randNum[i] = Math.floor((Math.random() * items.length));
-				while(randNum.indexOf(randNum[i]) != -1){
+				if(randNum.indexOf(randNum[i]) != -1){
 					randNum[i] = Math.floor((Math.random() * items.length));
 				}
 			};
